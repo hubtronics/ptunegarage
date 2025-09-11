cf9fa67575dfb03f403aed086cd80e34ec82e8ba
hubtronics <hubtronics.h@gmail.com>
2025-09-11 05:52:59 -0700

    upodated maps

--- public/script.js
+++ public/script.js
@@ -11,3 +11,14 @@ document.getElementById('bookingForm').onsubmit = function(e) {
     }
   });
 };
+
+// Map modal open/close code
+document.getElementById('openMap').onclick = function() {
+  document.getElementById('mapModal').style.display = 'flex';
+};
+document.getElementById('closeMap').onclick = function() {
+  document.getElementById('mapModal').style.display = 'none';
+};
+document.getElementById('mapModal').onclick = function(e) {
+  if (e.target === this) this.style.display = 'none';
+};
